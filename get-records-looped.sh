5f9aabe9d2467c038dc86e453b1ceeb859c195c9
German Lashevich <german.lashevich@gmail.com>
2018-01-24 10:43:22 +0100

    💄 Updating offset message and spinner

--- get-records-looped.sh
+++ get-records-looped.sh
@@ -13,10 +13,15 @@ LIMIT=$1; shift
 SHARD_ID=$(aws kinesis describe-stream --stream-name ${STREAM_NAME} | jq -r '.StreamDescription.Shards[0].ShardId')
 SHARD_ITERATOR=$(aws kinesis get-shard-iterator --stream-name ${STREAM_NAME} --shard-id ${SHARD_ID} --shard-iterator-type TRIM_HORIZON | jq -r '.ShardIterator')
 >&2 echo Quering records…
+i=1
+sp="/-\|"
 until [[ ${RECORDS} ]]; do
     RECORDS=$(aws kinesis get-records --shard-iterator ${SHARD_ITERATOR} --limit ${LIMIT})
+    printf "\b${sp:i++%${#sp}:1} "
     >&2 echo MillisBehindLatest: $(echo ${RECORDS} | jq -r '.MillisBehindLatest')
     SHARD_ITERATOR=$(echo ${RECORDS} | jq -r '.NextShardIterator')
     RECORDS=$(echo ${RECORDS} | jq '.Records[]')
+    echo -en "\x1B[1A";
 done
+echo 
 echo ${RECORDS}
